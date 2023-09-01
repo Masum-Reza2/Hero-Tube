1bce258514f430d804bf17590c6ca3ae899ace10
Masum-Reza <careerhunter4280@gmail.com>
2023-08-31 19:39:23 -0700

    dynamically button added.

--- js/engin.js
+++ js/engin.js
@@ -1,1 +1,28 @@
-console.log('engin connected')
+// setting all categories
+let loadData = async () => {
+    let res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    let data = await res.json();
+    setCategories(data)
+}
+
+loadData()
+
+let setCategories = (data) => {
+
+    let categoryParent = document.getElementById('categoryParent');
+    let categories = data.data;
+
+    categories.forEach(item => {
+        // console.log(item.category)
+        let button = document.createElement('button');
+        button.classList = 'btn btn-sm capitalize';
+        button.innerText = item.category;
+
+        categoryParent.appendChild(button)
+
+        button.addEventListener('click', ()=>{
+            // console.log(event.target.innerText)
+        })
+    });
+    
+}
